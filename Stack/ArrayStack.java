ab503807427613412c2b217f149fdbd153203edd
Rudra Kumar Dubey <rudradubey901@gmail.com>
2023-06-19 12:21:52 +0530

    commit

--- Stack/ArrayStack.java
+++ Stack/ArrayStack.java
@@ -25,31 +25,32 @@ public class ArrayStack {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Max Size of Stack");
-        int M = sc.nextInt();
-        int[] arr = new int[M];
-        int Top = M - 1;
-        Top = push(5, arr, Top);
-        Top = push(6, arr, Top);
-        Top = push(8, arr, Top);
-        Top = push(7, arr, Top);
-        Top = push(9, arr, Top);
-        Top = push(8, arr, Top);
-        System.out.println("After adding Arr is:");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-        Top = pop(arr, Top);
-        Top = pop(arr, Top);
-        Top = pop(arr, Top);
-        Top = pop(arr, Top);
-        Top = pop(arr, Top);
-        Top = pop(arr, Top);
-        Top = pop(arr, Top);
-        System.out.println("After Deleting Arr is:");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter Max Size of Stack");
+            int M = sc.nextInt();
+            int[] arr = new int[M];
+            int Top = M - 1;
+            Top = push(5, arr, Top);
+            Top = push(6, arr, Top);
+            Top = push(8, arr, Top);
+            Top = push(7, arr, Top);
+            Top = push(9, arr, Top);
+            Top = push(8, arr, Top);
+            System.out.println("After adding Arr is:");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+            }
+            Top = pop(arr, Top);
+            Top = pop(arr, Top);
+            Top = pop(arr, Top);
+            Top = pop(arr, Top);
+            Top = pop(arr, Top);
+            Top = pop(arr, Top);
+            Top = pop(arr, Top);
+            System.out.println("After Deleting Arr is:");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+            }
         }
     }
 }
--- Stack/CompareNum.java
+++ Stack/CompareNum.java
@@ -1,4 +1,3 @@
-import java.lang.*;
 import java.util.*;
 
 public class CompareNum {
@@ -8,15 +7,12 @@ public class CompareNum {
         Vector<String> arr = new Vector<String>(1);
         int j = 0;
         for (int i = 0; i < s.length(); i++) {
-            System.out.println("hi");
             if (Character.isDigit(s.charAt(i))) {
-                System.out.println("h2");
                 if (i + 1 >= s.length()) {
                     break;
                 } else {
                     k = k + s.charAt(i);
                     while (Character.isDigit(s.charAt(i + 1)) && i + 1 < s.length() - 1) {
-                        System.out.println("hello");
                         i++;
                         k = k + s.charAt(i);
                         if (i + 1 == s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
@@ -28,10 +24,8 @@ public class CompareNum {
                     k = "";
                     j++;
                 }
-                System.out.println("Here");
             }
         }
-        System.out.println("outtt");
         System.out.print(arr);
     }
 }
