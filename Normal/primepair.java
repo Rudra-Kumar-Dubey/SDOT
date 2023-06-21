a02ddd00184e644a785f3d3b72bd641fbe9968cc
Rudra Kumar Dubey <rudradubey901@gmail.com>
2023-06-21 12:18:22 +0530

    commit

--- Normal/primepair.java
+++ Normal/primepair.java
@@ -18,31 +18,31 @@ public class primepair {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int c = sc.nextInt();
-        int k = 0;
-        for (int i = n; i < m; i++) {
-            if (isPrime(i)) {
-                k++;
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int c = sc.nextInt();
+            int k = 0;
+            for (int i = n; i < m; i++) {
+                if (isPrime(i)) {
+                    k++;
+                }
             }
-        }
-        int a[] = new int[k];
-        k = 0;
-        for (int i = n; i < m; i++) {
-            if (isPrime(i)) {
-                a[k] = i;
-                k++;
+            int a[] = new int[k];
+            k = 0;
+            for (int i = n; i < m; i++) {
+                if (isPrime(i)) {
+                    a[k] = i;
+                    k++;
+                }
             }
-        }
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if (a[i] - a[j] == c) {
-                    System.out.println("{" + a[i] + ", " + a[j] + "}");
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a.length; j++) {
+                    if (a[i] - a[j] == c) {
+                        System.out.println("{" + a[i] + ", " + a[j] + "}");
+                    }
                 }
             }
         }
-
     }
 }
--- Normal/uniquedigits.java
+++ Normal/uniquedigits.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class uniquedigits {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int arr[] = new int[9];
-        for (int i = n; i <= m; i++) {
-            int t = i;
-            while (t > 0) {
-                arr[t % 10]++;
-                t = t / 10;
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int arr[] = new int[9];
+            for (int i = n; i <= m; i++) {
+                int t = i;
+                while (t > 0) {
+                    arr[t % 10]++;
+                    t = t / 10;
+                }
             }
-        }
-        int c = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) {
-                c++;
-                System.out.println(i);
+            int c = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == 1) {
+                    c++;
+                    System.out.println(i);
+                }
             }
+            System.out.println("Unique Digits: " + c);
         }
-        System.out.println("Unique Digits: " + c);
     }
 }
