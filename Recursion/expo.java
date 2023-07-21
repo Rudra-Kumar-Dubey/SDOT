4be65f29d6021e6c866e332c4baf6b56667f7f86
Rudra Kumar Dubey <rudradubey901@gmail.com>
2023-07-21 16:52:12 +0530

    recursion

--- Recursion/expo.java
+++ Recursion/expo.java
@@ -9,9 +9,24 @@ public class expo {
         return x * exp(x, n - 1);
     }
 
+    public static int logExpo(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return logExpo(x, n / 2) * logExpo(x, n / 2);
+        } else {
+            return x * logExpo(x, n / 2) * logExpo(x, n / 2);
+        }
+    }
+
     public static void main(String[] args) {
         int x = 2;
         int n = 10;
         System.out.println(exp(x, n));
+        System.out.println(logExpo(x, n));
     }
 }
