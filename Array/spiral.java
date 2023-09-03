cef2a02ece6ead1b89e77a2a83473935712aaf5b
Rudra Kumar Dubey <rudradubey901@gmail.com>
2023-09-03 14:55:59 +0530

    spiral

---
+++ Array/spiral.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class spiral {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return ans;
+        }
+        int row = matrix.length, col = matrix[0].length;
+        int left = 0, right = col - 1, top = 0, bottom = row - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        System.out.println(spiralOrder(arr));
+    }
+}
