9f82c478aa048bc848977e5668543426d6bbb249
Rudra Kumar Dubey <rudradubey901@gmail.com>
2023-09-05 17:41:44 +0530

    ransome

---
+++ Array/ransome.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ransome {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (map.containsKey(ransomNote.charAt(i)) && map.get(ransomNote.charAt(i)) > 0) {
+                map.put(ransomNote.charAt(i), map.get(ransomNote.charAt(i)) - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String ran = "a";
+        String mag = "b";
+        System.out.println(canConstruct(ran, mag));
+    }
+}
