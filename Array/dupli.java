293249b1b77406b15cb3a8a682d32ec08ee0d0c9
Rudra Kumar Dubey <rudradubey901@gmail.com>
2023-09-07 12:44:01 +0530

    dupli

--- Array/dupli.java
+++ Array/dupli.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 class dupli {
     public static boolean containsNearbyDuplicateNOP(int[] nums, int k) {
         for (int i = 0; i < nums.length; i++) {
@@ -11,9 +13,26 @@ class dupli {
         return false;
     }
 
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length < 2 || k == 0)
+            return false;
+        int i = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int j = 0; j < nums.length; j++) {
+            if (!set.add(nums[j])) {
+                return true;
+            }
+            if (set.size() >= k + 1) {
+                set.remove(nums[i++]);
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 1 };
         int k = 3;
         System.out.println(containsNearbyDuplicateNOP(nums, k));
+        System.out.println(containsNearbyDuplicate(nums, k));
     }
 }
