e1106dc34562c8897c12f75493e0ea1d476ac8ae
Rudra Kumar Dubey <rudradubey901@gmail.com>
2024-01-04 12:53:30 +0530

    tcs

---
+++ TCS Training/ques6.java
@@ -0,0 +1,17 @@
+public class ques6 {
+    public static void main(String[] arg) {
+        int n = 3;
+        int arr[] = { 30, 30, 60 };
+        int f = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 30 != 0) {
+                f = 1;
+                System.out.println("Transaction Not Successful");
+                break;
+            }
+        }
+        if (f == 0) {
+            System.out.println("Transaction Successful");
+        }
+    }
+}
