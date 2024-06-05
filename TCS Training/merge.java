7a92b62380341bc719beb8c3602cb29c83729f02
Rudra Kumar Dubey <rudradubey901@gmail.com>
2024-06-05 21:54:29 +0530

    tcs training

---
+++ TCS Training/merge.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class merge {
+    public static void mergeSort(int[] array) {
+        if (array.length <= 1) {
+            return;
+        }
+        int mid = array.length / 2;
+        int left[] = new int[mid];
+        int right[] = new int[array.length - mid];
+        System.arraycopy(array, 0, left, 0, left.length);
+        System.arraycopy(array, mid, right, 0, right.length);
+        mergeSort(left);
+        mergeSort(right);
+        merge(array, left, right);
+    }
+
+    public static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] arg) {
+        int[] array = { 17, 6, 7, 1, 9, 23 };
+        mergeSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
